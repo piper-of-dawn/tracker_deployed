95d500bf280b39830f8bb490d43a7fad3fe26410
piper-of-dawn <69080662+piper-of-dawn@users.noreply.github.com>
2021-11-23 13:23:49 +0530

    Initial commit

---
+++ precache-manifest.d402ee0ca2aca4469236b832441577f1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314ddd9bfc98ad26d323c46a270f5e5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81918ac7bf4ff257d726",
+    "url": "/static/css/main.ef001fa8.chunk.css"
+  },
+  {
+    "revision": "9d5b2417fffb0a369281",
+    "url": "/static/js/2.b78f5586.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/2.b78f5586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07ffb1d09ca612ceee4d",
+    "url": "/static/js/3.1626cf98.chunk.js"
+  },
+  {
+    "revision": "df8f0613abc92090a1cf1223439ada85",
+    "url": "/static/js/3.1626cf98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03c1142cb50c1e891db",
+    "url": "/static/js/4.6531ebfa.chunk.js"
+  },
+  {
+    "revision": "312b03fc2032f4a26db5e85de50ce98a",
+    "url": "/static/js/4.6531ebfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b51f4753d3358bd899",
+    "url": "/static/js/5.d34a35e1.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/static/js/5.d34a35e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81918ac7bf4ff257d726",
+    "url": "/static/js/main.728b2d4b.chunk.js"
+  },
+  {
+    "revision": "f9c0f1afdb9c139dc3f7",
+    "url": "/static/js/runtime-main.41db12e7.js"
+  }
+]);
---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+set url= --app="http://localhost:5000"
+start chrome %url% 
+calculator.py
+exit
